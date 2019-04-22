ef51f77ba14485bdef16bb6b07ae37fc588d5693
GD3 <GD3@DESKTOP-BSE2QAU>
2019-04-22 17:18:10 +0900

    변수 연습 1

---
+++ src/com/java0422/Main.java
@@ -0,0 +1,87 @@
+package com.java0422;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*System.out.println("변수 시작");
+
+		String 문자열 = "안녕하세요";
+
+		System.out.println(문자열);
+		// String 문자열선언; // 변수 만들기
+		// 문자열선언 = ""; // 변수의 값을 대입한다.
+
+		문자열 = "hi";
+
+		System.out.println(문자열); // 변수사용
+
+		문자열 = 문자열 + "(1234)"; // 연산
+
+		System.out.println(문자열);
+
+		byte b1 = -22;
+		short s1 = -220;
+		int i1 = 100;
+		long l1 = 1000;
+		System.out.println("b1 = " + b1);
+		System.out.println("s1 = " + s1);
+		System.out.println("i1 = " + i1);
+		System.out.println("l1 = " + l1);
+
+		float f = 0.1f;
+		double d = 10.5;
+		System.out.println("f = " + f);
+		System.out.println("d = " + d); 
+		
+		byte b1 = 10;
+		short s1 = 20;
+		int i1 = 100;
+		long l1 = 1000;
+		i1 = b1;
+		System.out.println("i1 = "+b1);
+		s1 = (short)l1;
+		System.out.println("s1 = "+l1);
+		float f = l1;
+		
+		System.out.println("l1 = "+f);
+		
+		int x=5, y =1;
+		System.out.println("++x = "+ (++x));
+		System.out.println("x++ = "+ (x++));
+		System.out.println("x-- = "+ (x--));
+		System.out.println("--x = "+ (--x)); 
+		
+		System.out.println(10/8);
+		System.out.println(10/-8);
+		System.out.println(-10/8);
+		System.out.println(-10/-8);
+		System.out.println(10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%8);
+		System.out.println(-10%-8); */
+		
+		char single = '\'';
+		String quote = "Hello \"홍길동!\"";
+		String root = "c:\\";
+		System.out.println(single);
+		System.out.println(quote);
+		System.out.println(root);
+		System.out.println("서울\t대구\t부산\t");
+		
+		System.out.println("변수 시작!");
+		String 문자열 = "Hello World"; // 변수 선언 과 대입이 동시에 이루어진거.
+		System.out.println(문자열); // 변수 사용
+		문자열 = "문자열 변경";   // 값 변경
+		System.out.println(문자열); // 변수 사용
+		문자열 = "1234"; // 연산
+		System.out.println(문자열); // 변수 사용
+		
+		int 정수 = 1;
+		System.out.println(정수);
+		
+		String 결과값 = 문자열 + (정수 + 정수 + 정수);
+		System.out.println(결과값);
+
+	}
+
+}
