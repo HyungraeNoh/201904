52c587d8c2f19b75a3b1e6e8a867f4ab1baaf049
GD3 <GD3@DESKTOP-BSE2QAU>
2019-04-23 16:30:27 +0900

    메소드 연습(계산기)

--- src/com/java0423/Calc.java
+++ src/com/java0423/Calc.java
@@ -1,16 +1,42 @@
 package com.java0423;
 
+class Calc1 {
+
+	int 더하기(int a, int b) {
+
+		return a + b;
+	}
+
+	int 빼기(int a, int b) {
+
+		return a - b;
+	}
+
+	int 곱하기(int a, int b) {
+
+		return a * b;
+	}
+
+	int 나누기(int a, int b) {
+
+		return a / b;
+	}
+	
+	int 나머지(int a, int b) {
+
+		return a % b;
+	}
+}
+
 public class Calc {
 
 	public static void main(String[] args) {
-		
-		System.out.println(sum(5,2));
+		Calc1 c = new Calc1();
+
+		System.out.println("a+b = " + c.더하기(5, 2));
+		System.out.println("a-b = " + c.빼기(5, 2));
+		System.out.println("a*b = " + c.곱하기(5, 2));
+		System.out.println("a/b = " + c.나누기(5, 2));
+		System.out.println("a%b = " + c.나머지(5, 2));
 	}
-
-	static int sum(int a, int b) {
-
-		return a+b;
-
-	}
-
 }
