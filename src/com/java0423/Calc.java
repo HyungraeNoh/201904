6345e61d9227c3e132e63717025d8452f6de96cb
GD3 <GD3@DESKTOP-BSE2QAU>
2019-04-23 16:07:49 +0900

    메소드 연습(계산기)

---
+++ src/com/java0423/Calc.java
@@ -0,0 +1,16 @@
+package com.java0423;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		System.out.println(sum(5,2));
+	}
+
+	static int sum(int a, int b) {
+
+		return a+b;
+
+	}
+
+}
